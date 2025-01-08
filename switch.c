544fd4e4e7a8159ba81bfef37e85ee272dac0625
Arya-5002 <bhattacharjeearya746@gmail.com>
2025-01-08 22:23:27 +0530

    new

---
+++ switch.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int year ;
+    scanf("%d",&year);
+    switch(year){
+        case 1: 
+        printf("Physics");
+        printf("Chemistry");
+        printf("Maths");
+
+        case 2:
+        char branch ;
+        scanf("%c",&branch);
+    }
+}
